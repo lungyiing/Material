d552e7b1992c0331cf96190251a34a16986227e4
Christopher Ireland <ireland63@gmail.com>
2016-05-25 11:20:16 +0100

    Scss-lint

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
 $ThemeInfo['material'] = array(
     'Name' => 'Material',
     'Description' => 'Inspired by Material Design (overrides mobile theme selection on install)',
-    'Version' => '0.7.0',
+    'Version' => '0.8.0',
     'Author' => 'Chris Ireland',
     'AuthorEmail' => 'christopher.ireland@me.com',
     'License' => 'MIT'
